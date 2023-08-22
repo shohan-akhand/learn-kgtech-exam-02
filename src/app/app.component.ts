b9559ca9a71bca7754d6cfb4b46e2b89399e643e
shohan akhand <shohanakhand@gmail.com>
2023-08-22 14:31:59 +0600

    complete first task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'kgtech-test02';
+  kilobyte = 0;
+  onInput = (event: Event) => {
+    const value = parseFloat((<HTMLInputElement>event.target).value);
+    this.kilobyte = value;
+  };
 }
---
+++ src/app/convert.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convert',
+})
+export class ConvertPipe implements PipeTransform {
+  transform(value: number, targetUnits: string): any {
+    if (!value) {
+      return 0;
+    }
+    switch (targetUnits) {
+      case 'mb':
+        return value / 1024;
+      case 'gb':
+        return value / 1048576;
+    }
+  }
+}
